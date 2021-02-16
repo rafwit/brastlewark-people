5f780ffd8980ec0a5fa6b84ba1bc1f9a21930d8d
Rafal Witczak <rwitczak89@gmail.com>
2021-02-16 17:06:30 +0100

    feat(persondetails.js): add detail info to PersonDetails component
    in addition, create a links to friends details pages

--- src/components/PersonDetails.js
+++ src/components/PersonDetails.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+
+import Avatar from "react-avatar";
+
 import Breadcrumbs from "./Breadcrumbs";
 
 export default function PersonDetails() {
@@ -18,11 +21,56 @@ export default function PersonDetails() {
           { label: person.name },
         ]}
       />
-
+      {console.log(person)}
       <div className="person_details">
-        <div className="person__avatar"></div>
-        <div className="person__name">{person.name}</div>
-        <div className="person__details"></div>
+        <Avatar src={person.thumbnail} round={true} size={"10rem"} />
+        <div className="person_details__name">{person.name}</div>
+        <div className="person_details__details">
+          <div className="person_details__details--age"></div>
+          <div className="person_details__details--weight"></div>
+          <div className="person_details__details--height"></div>
+          <div className="person_details__details--hair_color"></div>
+        </div>
+        <div className="person_details__professions">
+          {person.professions.map((profession) => {
+            return (
+              <div
+                className="person_details__professions--profession"
+                key={`${person.id}${person.height}${profession}`}
+              >
+                {profession}
+              </div>
+            );
+          })}
+          <div className="person_details__friends">
+            {person.friends
+              ? person.friends.map((friend) => {
+                  const [fullFriendInfo] = people.filter((person) => {
+                    if (person.name.includes(friend)) {
+                      return true;
+                    }
+                  });
+                  const name = fullFriendInfo
+                    ? fullFriendInfo.name.split(" ")
+                    : undefined;
+
+                  return (
+                    <Link
+                      to={
+                        fullFriendInfo
+                          ? `/people/${fullFriendInfo.id}/${name.join("")}`
+                          : "#"
+                      }
+                      className="person_details__friends--friend"
+                      key={`${person.id}${person.height}${friend}`}
+                    >
+                      {friend}
+                    </Link>
+                  );
+                })
+              : null}
+          </div>
+        </div>
       </div>
     </>
   );
