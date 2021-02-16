42c09fbf884d6f48ff539e97cce56ad9b5ba7c80
Rafal Witczak <rwitczak89@gmail.com>
2021-02-16 01:10:45 +0100

    feat(people.js): fetch data and render all people in people component

--- src/components/People.js
+++ src/components/People.js
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+
+import { getPeople } from "../store/actions";
 
 export default function People() {
-  return <div className="people">PEOPLE </div>;
+  const people = useSelector((store) => store.people);
+  const distpach = useDispatch();
+
+  useEffect(() => {
+    people ? distpach(getPeople()) : null;
+  }, []);
+
+  return (
+    <div className="people">
+      {people
+        ? people.map((person) => {
+            return <PersonCard person={person} key={person.id} />;
+          })
+        : null}
+    </div>
+  );
+}
+
+function PersonCard({ person }) {
+  return (
+    <Link className="person" to={`people/${person.id}`}>
+      <div className="person__avatar"></div>
+      <div className="person__name">{person.name}</div>
+      <div className="person__details"></div>
+    </Link>
+  );
 }
