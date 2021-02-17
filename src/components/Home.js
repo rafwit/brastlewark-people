624d275e3c3c34726186f74231feda2a5913d900
Rafal Witczak <rwitczak89@gmail.com>
2021-02-18 00:27:32 +0100

    feat(search bar): extract search bar from Home component
    it will be used in People component

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,16 +5,6 @@ export default function Home() {
   return (
     <>
       <div className="homepage">
-        <div className="homepage__search_bar">
-          <div className="homepage__search_bar--title">Find person by name</div>
-          <form className="homepage__search_bar--form">
-            <input className="homepage__search_bar--input" type="text" />
-            <button className="homepage__search_bar--button" type="submit">
-              Search
-            </button>
-          </form>
-        </div>
-        <div className="homepage__separator">OR</div>
         <Link to="/people" className="homepage__link">
           <div className="homepage__link--title">
             Browse all Brastlewark&apos;s People &rarr;
